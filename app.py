8ff70a4df444bcd918d4c88dd3ae5fba1921f658
Andrew VanNess <avanness@student.bridgew.edu>
2018-12-20 22:29:37 -0500

    use postgresql instead of sqlite

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
 
 app = Flask(__name__)
-chatterbot = ChatBot("Jamie", read_only=False)
+chatterbot = ChatBot("Jamie",
+        storage_adapter = "chatterbot.storage.SQLStorageAdapter",
+        read_only=False,
+        database_uri = os.environ['DATABASE_URL']
+        )
 
 @app.route('/chatter/<phrase>', methods=['GET'])
 def note_page(phrase):
