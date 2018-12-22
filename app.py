df01bf2b076e9c4635af166073c9383f4143cc56
Andrew VanNess <avanness@student.bridgew.edu>
2018-12-22 01:10:26 -0500

    use try/catch

--- app.py
+++ app.py
@@ -5,15 +5,30 @@ import os
 
 app = Flask(__name__)
 chatterbot = ChatBot("Jamie",
-        storage_adapter = "chatterbot.storage.SQLStorageAdapter",
-        read_only=False,
-        database_uri = os.environ['DATABASE_URL']
-        )
+                     storage_adapter="chatterbot.storage.SQLStorageAdapter",
+                     read_only=False,
+                     database_uri=os.environ['DATABASE_URL']
+                     )
+
+
+# To train with default english corpus
+
+# chatterbot.set_trainer(ChatterBotCorpusTrainer)
+
+# chatterbot.train(
+#     "chatterbot.corpus.english"
+# )
+
+###
 
 @app.route('/chatter/<phrase>', methods=['GET'])
 def note_page(phrase):
-    response = str((chatterbot.get_response(phrase)))
-    return(response)
+    try:
+        response = str((chatterbot.get_response(phrase)))
+        return(response)
+    except:
+        return("error getting response")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
